db8dd549270071b1cea78a03bdd613699e336fed
Iain <jtfjyrx@gmail.com>
2022-03-04 17:39:36 +0000

    broken

--- main.py
+++ main.py
@@ -11,19 +11,24 @@ class room:
         window.addstr(int(x), int(y), self.i)
 
 def main(window):
+    start = room("s")
+    connector = room("x")
+    boss = room("b")
+    treasure = room("t")
+
     length = random.randint(8, 30)
     curses.noecho()
     curses.curs_set(0)
     window.keypad(1)
     curses.use_default_colors()
+
     win_width = (window.getmaxyx()[1] - 1) # y
     win_height = (window.getmaxyx()[0] - 1) # x
     startPosX = (win_height / 2)
     startPosY = (win_width / 2)
     x = startPosX
     y = startPosY
-    start = room("s")
-    connector = room("x")
+
     start.spawn(window, x, y)
     window.box()
     window.refresh()
@@ -37,7 +42,7 @@ def main(window):
         if key == "q":
             sys.exit(0)
 
-        elif key == "a":
+        elif key == "a": # how do I check if something is already there?
             direction = random.randint(0,1)
             mult = random.randint(0,1)
             if mult == 0:
@@ -46,6 +51,15 @@ def main(window):
                 x = (x + mult)
             else:
                 y = (y + mult)
-            connector.spawn(window, x, y)
+
+            attrs = window.inch(int(y), int(x))
+            ch = chr(attrs & 255)
+            isbold = bool(attrs & curses.A_BOLD)
+            if isbold: # this just locks everything out
+                pass
+            # if char in ["x", "b", "t", "s"]: # this seems to do a whole lot of nothing
+                # pass
+            else:
+                connector.spawn(window, x, y)
 
 wrapper(main)
